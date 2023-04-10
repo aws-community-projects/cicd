1bf9a16232669a75a67fa50a9aa6209d5bba62e4
Matt Morgan <elthrasher@gmail.com>
2023-04-10 16:44:16 -0400

    fix test

--- test/setup.ts
+++ test/setup.ts
@@ -2,5 +2,8 @@ import { expect } from 'vitest';
 
 expect.addSnapshotSerializer({
   serialize: (val) => JSON.stringify({ ...val, S3Key: '[HASH REMOVED].zip' }),
-  test: (val) => val && Object.prototype.hasOwnProperty.call(val, 'S3Key'),
+  test: (val) =>
+    val &&
+    (Object.prototype.hasOwnProperty.call(val, 'S3Key') ||
+      Object.prototype.hasOwnProperty.call(val, 'SourceObjectKeys')),
 });
