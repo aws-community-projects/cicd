6a4096a9d14e31a48adac3e3d88f797e4f31757f
Matt Morgan <elthrasher@gmail.com>
2023-04-10 16:38:14 -0400

    capitalization

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <div className="App-content">hello community!</div>
+      <div className="App-content">Hello community!</div>
     </div>
   );
 }
